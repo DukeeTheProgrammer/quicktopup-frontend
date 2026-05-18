0c67bab9e3e8c4c2d86576ffcd76b50caac3e130
Dukee TheProgrammer <ujiroduke1@gmail.com>
2026-05-18 23:08:38 +0100

    update src/api/wallet.js

--- src/api/wallet.js
+++ src/api/wallet.js
@@ -4,3 +4,5 @@ export const getWallet = () => client.get('/wallet/');
 export const fundWallet = (data) => client.post('/wallet/fund/', data);
 export const fundWalletCallback = (params) => client.get('/wallet/fund/callback/', { params });
 export const getWalletLedger = (params) => client.get('/wallet/ledger/', { params });
+// NEW: poll funding status by reference (3.a in API docs)
+export const getFundingStatus = (reference) => client.get(`/wallet/funding/${reference}/`);
